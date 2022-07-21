6672a937b83b8269aaf3cee85201d11b1bff76bb
TommyHacker <t.hacker.webdev@gmail.com>
2022-07-21 21:09:58 +0100

    added dotenv

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const cors = require("cors");
-const path = require("path");
+const cors = require('cors');
+const path = require('path');
+const dotenv = require('dotenv').config();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("server is open");
+app.get('/', (req, res) => {
+	res.send('server is open');
 });
 
 module.exports = app;
