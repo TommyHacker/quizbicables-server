0b3cc2b2ebb91ad060795ef64e286afac5e1da7c
TommyHacker <t.hacker.webdev@gmail.com>
2022-07-24 18:36:15 +0100

    removed console logs

--- index.js
+++ index.js
@@ -65,7 +65,6 @@ io.on('connection', async (socket) => {
 
 		socket.on('update_score', async ({ data }) => {
 			const newPlayersList = await data;
-			console.log('correct answer,', data);
 			// send the updated score to all players in this room
 			io.to(roomNum).emit('players', { data });
 		});
